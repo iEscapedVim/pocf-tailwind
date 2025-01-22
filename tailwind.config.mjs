8c7a03b1d86bdb511414ebbf1ece5e95677dd203
iEscapedVim <mail>
2025-01-22 11:46:02 +0500

    sliding members and images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,6 +13,15 @@ export default {
 				'white': '#ffffff',
 				'black': '#000000',
 			},
+			animation: {
+				'infinite-scroll': 'infinite-scroll 25s linear infinite',
+			},
+			keyframes: {
+				'infinite-scroll': {
+				from: { transform: 'translateX(0)' },
+				to: { transform: 'translateX(-100%)' },
+				}
+			}                    
 		},
 	},
 	plugins: [
